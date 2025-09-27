216ba287e3e0c6530c7de60249b80cf778e9086d
hw <hussainweb@gmail.com>
2025-09-26 21:24:54 -0400

    feat: Add new git abbreviations
    Introduces the following new git abbreviations:
    - `gfo`: `git fetch origin`
    - `ga.`: `git add .`
    - `gd.`: `git diff .`

--- dot_config/fish/private_conf.d/gitabbr.fish
+++ dot_config/fish/private_conf.d/gitabbr.fish
@@ -1,12 +1,15 @@
 abbr g git
 abbr gf git fetch
 abbr gfa git fetch --all
+abbr gfo git fetch origin
 abbr gs git status
 abbr gs. git status .
 abbr gc git commit -m
 abbr gm git merge FETCH_HEAD --ff-only
 abbr gr git rb FETCH_HEAD
 abbr ga git add
+abbr ga. git add .
 abbr gp git push
 abbr gd git diff
+abbr gd. git diff .
 abbr gco git checkout
