1a04b5ba2d683da00f9613c8653ae6379cfc4965
hw <hussainweb@gmail.com>
2025-09-26 21:21:04 -0400

    feat: Add git abbrations
    This commit introduces a new file `dot_config/fish/private_conf.d/gitabbr.fish` which defines several fish shell abbreviations for common git commands. This will allow for faster and more convenient usage of git.

---
+++ dot_config/fish/private_conf.d/gitabbr.fish
@@ -0,0 +1,12 @@
+abbr g git
+abbr gf git fetch
+abbr gfa git fetch --all
+abbr gs git status
+abbr gs. git status .
+abbr gc git commit -m
+abbr gm git merge FETCH_HEAD --ff-only
+abbr gr git rb FETCH_HEAD
+abbr ga git add
+abbr gp git push
+abbr gd git diff
+abbr gco git checkout
